de2e8a4a903a30784a87cc7e0ef12298ea3b395f
Mikhail Borisov <dsgnmike@gmail.com>
2020-08-22 16:26:58 +0300

    project init

---
+++ DangerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DangerTest
+//
+//  Created by Михаил Борисов on 22.08.2020.
+//  Copyright © 2020 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
